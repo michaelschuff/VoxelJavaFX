afd592ece968c28020156b06ce573c2423af4e8a
Michael Schuff <mschuff0881@gmail.com>
2020-08-16 16:04:02 -0700

    remopved some lines

--- Testing_Experimentation/src/sample/helloworld.java
+++ Testing_Experimentation/src/sample/helloworld.java
@@ -17,7 +17,6 @@ public class helloworld extends Application {
     public void start(Stage primaryStage) throws Exception{
         System.out.println("hello world");
 
-        System.out.println('h');
         double gastropod = 5;
         System.out.println("hi 2.0");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
