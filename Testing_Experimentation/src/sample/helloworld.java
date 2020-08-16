2e1d660bbab745bd4702aa035f9c28f1dbcfa0f5
Michael Schuff <mschuff0881@gmail.com>
2020-08-16 15:06:50 -0700

    no message

--- Testing_Experimentation/src/sample/helloworld.java
+++ Testing_Experimentation/src/sample/helloworld.java
@@ -7,12 +7,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class helloworld extends Application {
-    
+
+
+    public double EEEEEE = 5;
     @Override
     public void start(Stage primaryStage) throws Exception{
         System.out.println("hello world");
+
+        System.out.println("hi 2.0");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello World Pnis");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
